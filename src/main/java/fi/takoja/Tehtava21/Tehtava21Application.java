4953990b887ebfa39d073c90189e96cbf9fa9655
Niko Takoja <niko@takoja.fi>
2021-09-06 18:19:46 +0300

    Ensimmäinen

---
+++ src/main/java/fi/takoja/Tehtava21/Tehtava21Application.java
@@ -0,0 +1,13 @@
+package fi.takoja.Tehtava21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tehtava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tehtava21Application.class, args);
+	}
+
+}
---
+++ src/test/java/fi/takoja/Tehtava2/Tehtava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.takoja.Tehtava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tehtava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
